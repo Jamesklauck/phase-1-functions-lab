c3641263070d5e9baf5985bc207067782a682083
James <jamesklauck@Jamess-MacBook-Air.local>
2021-12-14 08:25:42 -0500

    commit tree 8150a47859dc90488ce2e27622d0d45487b6617c

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const theHq = 42;
+function distanceFromHqInBlocks(theHq, number2) {
+    if (theHq < number2) {
+    return (number2 - theHq);
+    }
+    else {
+        return (theHq - number2);
+    }
+}
+distanceFromHqInBlocks(theHq, 50)
